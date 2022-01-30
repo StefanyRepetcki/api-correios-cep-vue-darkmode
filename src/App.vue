e3534f877e5ec0b9be953163f7b8f26ea3d00f06
Stefany Repetcki <stefany.repetcki@contabilizei.com.br>
2022-01-30 17:28:44 -0300

    feat(projeto): app.vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <core-app-bar />
+
+    <core-view />
+
+    <core-footer />
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+
+    components: {
+      CoreFooter: () => import('@/components/core/Footer'),
+      CoreAppBar: () => import('@/components/core/AppBar'),
+      CoreView: () => import('@/components/core/View'),
+    },
+  }
+</script>
